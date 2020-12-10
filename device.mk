a9666a8fd347a125e827abd09f0b7bd908cf422a
Pig <pig.priv@gmail.com>
2020-12-11 06:26:23 +0800

    violet: Re-enable some audio features
     * Drop unused ones while at it.
    
    Change-Id: I5413e1ffb6a35dd8aea97801f08292889b82304b

--- device.mk
+++ device.mk
@@ -92,9 +92,6 @@ PRODUCT_PACKAGES += \
     audio.usb.default \
     liba2dpoffload \
     libaudio-resampler \
-    libbatterylistener \
-    libcomprcapture \
-    libexthwplugin \
     libhdmiedid \
     libhfp \
     libqcompostprocbundle \
